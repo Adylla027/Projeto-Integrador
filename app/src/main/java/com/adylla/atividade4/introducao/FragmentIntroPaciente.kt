942496ba5d31722f5245f5f10daee817d2b9be58
nagitodst <ecaros14@gmail.com>
2025-09-04 17:33:41 -0300

    finalizei a tela de introdução do profissional no arquivo fragment_intro_profissional.xml // adicionei o texto da tela de introdução no arquivo strings.xml // mudei a cor dos botoes "paciente" e "profissional" no fragment_introducao.xml

---
+++ app/src/main/java/com/adylla/atividade4/introducao/FragmentIntroPaciente.kt
@@ -0,0 +1,33 @@
+package com.adylla.atividade4.introducao
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.adylla.atividade4.databinding.FragmentIntroPacienteBinding
+
+
+class FragmentIntroPaciente : Fragment() {
+    private var _binding: FragmentIntroPacienteBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentIntroPacienteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
